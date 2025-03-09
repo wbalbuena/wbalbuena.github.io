77354364bc420f58a08e1e181424c40a347fe105
wbalbuena <w_balbuena@u.pacific.edu>
2025-03-09 11:02:20 -0700

    Fixed bug where routing path to unused component was still in code

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.jsx'
-import Projects from './Projects.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
