b52892efd71185cfdfd5c1fc3ae5d45c1a775ad5
wbalbuena <w_balbuena@u.pacific.edu>
2025-03-09 00:03:45 -0800

    Removed unused components

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.jsx'
-import About from './About.jsx'
 import Projects from './Projects.jsx'
 
 createRoot(document.getElementById('root')).render(
