3ec461eb996ef25d48c1c2e04b770f54d7da120b
wbalbuena <w_balbuena@u.pacific.edu>
2025-06-15 00:48:42 -0700

    Updated layout (note: update for mobile layouts)

--- src/App.jsx
+++ src/App.jsx
@@ -16,32 +16,45 @@ export default function App() {
         <a href="https://www.linkedin.com/in/william-balbuena/" target="_blank" ><img src="https://img.icons8.com/?size=100&id=13930&format=png&color=000000" className="icon"/></a>
         <a href="https://github.com/wbalbuena" target="_blank" ><img src="https://img.icons8.com/?size=100&id=iEBcQcM9rnZ9&format=png&color=000000" className="icon"/></a>
       </div>
-      <div className="twocolumns">
-        <div className="cards">
-          <div className="card">
-            <b>about 🎓</b>
-            <p>BS in Computer Science from <span class="text-[#F47821]">University of the Pacific</span></p>
+      <div className="container">
+        <div className="twocolumns">
+          <div className="cards">
+            <div className="card">
+              <b>about 🎓</b>
+              <p>BS in Computer Science from <span class="text-[#F47821]">University of the Pacific</span></p>
+            </div>
+            <div className="card">
+              <b>experiences 👔</b>
+              <p>ASIC Characterization Junior Engineer @ <span class="text-[#0078C1]">Micron Technology</span></p>
+              <p>Mustang Tech Squad @ <span class="text-[#fac624]">San Joaquin Delta College</span>, IT Department</p>
+            </div>
+            <div className="card">
+              <b>awards 🥇</b>
+              <p>Valedictorian @ <span class="text-[#F47821]">University of the Pacific</span></p>
+              <p>1st @ 2021 SJCOE <span class="text-blue-400">H2O Hackathon</span></p>
+            </div>
+            <div className="card">
+              <b>languages 🗣</b>
+              <p>I like coding with <span class="text-[#FFD43B]">Python</span>, <span class="text-[#336791]">SQL</span>, <span class="text-[#00599C]">C++</span></p>
+              <p>I've had some work with: <span class="text-[#217346]">VBA</span>, <span class="text-[#6E4C13]">Assembly</span>, <span class="text-[#A8B9CC]">C</span>, <span class="text-[#E76F00]">Java</span>, <span class="text-[#E34F26]">HTML</span> / <span class="text-[#1572B6]">CSS</span>, <span class="text-[#F7DF1E]">JavaScript</span></p>
+            </div>
+            <div className="card">
+              <b>technologies 💻</b>
+              <p>Flask, <span class="text-[#61DAFB]">React</span>, <span class="text-[#43B02A]">Selenium</span>, SQLite, PostgreSQL, <span class="text-[#199F4B]">vim</span></p>
+            </div>
+            <div className="card">
+              <b>misc</b>
+              <p>104 WPM certified typing speed</p>
+            </div>
           </div>
           <div className="card">
-            <b>experiences 👔</b>
-            <p>ASIC Characterization Junior Engineer @ <span class="text-[#0078C1]">Micron Technology</span></p>
-            <p>IT General Helper @ <span class="text-[#fac624]">San Joaquin Delta College</span>, IT Department</p>
+            <b>currently reading 📚</b>
+            <div className="bookshelf">
+              <GoodreadsBookshelf userId="188900711-william" shelf="currently-reading" width="91.45px" />
+            </div>
           </div>
-          <div className="card">
-            <b>awards 🥇</b>
-            <p>Valedictorian @ <span class="text-[#F47821]">University of the Pacific</span></p>
-            <p>1st @ 2021 SJCOE <span class="text-blue-400">H2O Hackathon</span></p>
-          </div>
-          <div className="card">
-            <b>languages 🗣</b>
-            <p>I like coding with <span class="text-[#FFD43B]">Python</span>, <span class="text-[#336791]">SQL</span>, <span class="text-[#00599C]">C++</span></p>
-            <p>I've had some work with: <span class="text-[#217346]">VBA</span>, <span class="text-[#6E4C13]">Assembly</span>, <span class="text-[#A8B9CC]">C</span>, <span class="text-[#E76F00]">Java</span>, <span class="text-[#E34F26]">HTML</span> / <span class="text-[#1572B6]">CSS</span>, <span class="text-[#F7DF1E]">JavaScript</span></p>
-          </div>
-          <div className="card">
-            <b>technologies 💻</b>
-            <p>Flask, <span class="text-[#61DAFB]">React</span>, <span class="text-[#43B02A]">Selenium</span>, SQLite, PostgreSQL, <span class="text-[#199F4B]">vim</span></p>
-          </div>
-          <div className="card">
+        </div>
+        <div className="bottom">
             <b>projects 🎨</b>
             <div className="projects">
             {
@@ -56,17 +69,6 @@ export default function App() {
               ))
             }
             </div>
-          </div>
-          <div className="card">
-            <b>misc</b>
-            <p>104 WPM certified typing speed</p>
-          </div>
-        </div>
-        <div className="card">
-          <b>currently reading 📚</b>
-          <div className="bookshelf">
-            <GoodreadsBookshelf userId="188900711-william" shelf="currently-reading" width="91.45px" />
-          </div>
         </div>
       </div>
     </>
